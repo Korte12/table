5529885478e42cf8346857b59a285a97e0669a87
Kortvelyesi Roland <kortvelyesi.roland@BOLYAI.LOCAL>
2024-10-11 12:57:56 +0200

    js parentElement

--- main.js
+++ main.js
@@ -27,7 +27,6 @@ let array = [
         pet: 'macska'
     },
 ]
-
 const table = document.createElement('table')
 document.body.appendChild(table)
 
@@ -43,107 +42,144 @@ tableheadrow.appendChild(th)
 const th2 = document.createElement('th')
 tableheadrow.appendChild(th2)
 
-const thazas = document.createElement('th')
-tableheadrow.appendChild(thazas)
+const th3 = document.createElement('th')
+tableheadrow.appendChild(th3)
 
-const thallat = document.createElement('th')
-tableheadrow.appendChild(thallat)
+const th4 = document.createElement('th')
+tableheadrow.appendChild(th4)
 
-th.innerHTML="Vezetéknév"
-th2.innerHTML="Keresztnév"
-
-thazas.innerHTML="Házas-e"
-thallat.innerHTML="Házi állat"
-
+th.innerHTML = "Veznev"
+th2.innerHTML = "Kernev"
 th2.colSpan = 2
-
-
+th3.innerHTML = "Házas-e"
+th4.innerHTML = "Háziállat"
 
 const tablebody = document.createElement('tbody')
 table.appendChild(tablebody)
-for(const person of array){
-    const tr = document.createElement('tr')
-    const lastname = document.createElement('td')
-    tablebody.appendChild(tr)
-    tr.appendChild(lastname)
-    lastname.innerHTML = person.lastname
 
-    const firstname = document.createElement('td')
-    tablebody.appendChild(tr)
-    tr.appendChild(firstname)
-    firstname.innerHTML = person.firstname1
+const form = document.getElementById('form')
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const lastname = document.getElementById('lastname')
+    const firstname = document.getElementById('firstname1')
+    const firstname2 = document.getElementById('firstname2')
+    const pet = document.getElementById('pet')
+    const married = document.getElementById('married')
 
-    if(person.firstname2===undefined){
-        firstname.colSpan = 2
-    }
-    else{
-        const firstname2 = document.createElement('td')
-        firstname2.innerHTML = person.firstname2
-        tr.appendChild(firstname2)
+    const lastnamevalue = lastname.value
+    const firstnamevalue = firstname.value
+    let firstname2value = firstname2.value
+    const marriedvalue = married.checked
+    const petvalue = pet.value
 
-    }
+    if (firstname2value === ""){
+        firstname2value = undefined
 
-    const married = document.createElement('td')
-    tablebody.appendChild(tr)
-    tr.appendChild(married)
-    married.innerHTML = person.married
-
-    if (person.married === true) {
-        married.innerHTML = "igen"
-    }
-    else {
-        married.innerHTML = "nem"
     }
 
-    const pet = document.createElement('td')
-    tablebody.appendChild(tr)
-    tr.appendChild(pet)
-    pet.innerHTML = person.pet
-    
-    
-    tablebody.appendChild(tr)
-    
 
-
-
-
-    tr.addEventListener('click', function(e)
+    const newperson =
     {
-        let valasz = tablebody.querySelector('.selected')
+        firstname1: firstnamevalue,
+        firstname2: firstname2value,
+        lastname: lastnamevalue,
+        married: marriedvalue,
+        pet: petvalue
 
-        if (valasz != undefined)
-        {
-            valasz.classList.remove('selected');
+    }
+    if(validateFields(lastname, firstname, pet))
+    array.push(newperson)
+    console.log(array)
+    tablebody.innerHTML = ""
+    rendertable()
+
+    
+})
+function validateFields(lastname, firstname, pet) {
+    let result = true
+    if (lastname.value == "") {
+        const par = lastname.parentElement
+        const error = par.querySelector('.error')
+        error.innerHTML = "Kötelező vezetéknév"
+        result = false
+    }
+
+    if (lastname.value == "") {
+        const par1 = firstname.parentElement
+        const error1 = par1.querySelector('.error')
+        error1.innerHTML = "Kötelező vezetéknév"
+        result = false
+    }
+
+    if (lastname.value == "") {
+        const par2 = pet.parentElement
+        const error2 = par2.querySelector('.error')
+        error2.innerHTML = "Kötelező vezetéknév"
+        result = false
+    }
+    return result
+}
+rendertable()
+function rendertable() {
+    for (const person of array) {
+        const tr = document.createElement('tr')
+
+        const lastname = document.createElement('td')
+        tablebody.appendChild(tr)
+        tr.appendChild(lastname)
+        lastname.innerHTML = person.lastname
+
+        const firstname = document.createElement('td')
+        tablebody.appendChild(tr)
+        tr.appendChild(firstname)
+        firstname.innerHTML = person.firstname1
+
+
+
+
+        tablebody.appendChild(tr)
+
+
+        if (person.firstname2 === undefined) {
+            firstname.colSpan = 2
         }
-        
-        console.log(e); 
-        e.currentTarget.classList.add('selected');
+        else {
+            const firstname2 = document.createElement('td')
+            firstname2.innerHTML = person.firstname2
+            tr.appendChild(firstname2)
 
-        
-    })
+        }
 
+        const married = document.createElement('td')
+        tablebody.appendChild(tr)
+        tr.appendChild(married)
+        married.innerHTML = person.married
+
+        const pet = document.createElement('td')
+        tablebody.appendChild(tr)
+        tr.appendChild(pet)
+        pet.innerHTML = person.pet
+
+        if (person.married === true) {
+            married.innerHTML = "igen"
+        } else {
+            married.innerHTML = "nem"
+        }
+
+
+        tr.addEventListener('click', function (e) {
+            console.log('click')
+
+            const ez = tablebody.querySelector('.selected')
+            if (ez != undefined) {
+                ez.classList.remove('selected')
+            }
+            e.currentTarget.classList.add('selected')
+        })
+
+    }
     
-    
+ 
 
 }
-const form = document.getElementById("form")
-form.addEventListener('submit', 
-    function(e) 
-    {
-        e.preventDefault()
-        let lastn = document.getElementById('lastname')
-        let firstn1 = document.getElementById('firstname1')
-        let firstn2 = document.getElementById('firstname2')
-        let marry = document.getElementById('married')
-        let pett = document.getElementById('pet')
-
-        const lastnValue = lastmane.value
-        const firstn1Value = firstname1.value
-        const firstn2Value = firstname2.value
-        const marryValue = married.check
-        const pettValue = pet.value
-
-        
-    })
-
 
